6be6359e2ad66500aa9060c1d72e7752e59b67ba
Rob Porto <robson@loft47.com>
2024-08-30 14:23:53 -0700

    Make test fail

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,9 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+
+    // expect to find a text saying blablabla
+    cy.get('h1').should('have.text', 'blablabla')
+
   })
 })
